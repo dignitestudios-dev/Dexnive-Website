88fef8e5112d47b4c8681d6fa66c27e3a63da3f2
huzaifa-koder <huzaifa.tanveer@launchbox.pk>
2026-07-17 19:57:42 +0500

    fix issues in bpo

--- app/(main)/bpo-services/page.tsx
+++ app/(main)/bpo-services/page.tsx
@@ -4,7 +4,7 @@ import BpoSolutions from "./_components/bpo-solutions";
 export const metadata: Metadata = {
   title: "Leading Business Process Outsourcing (BPO) Services",
   description:
-    "Streamline your operations with Business Process Outsourcing (BPO) services by Dignite Studios designed to reduce usual costs and improve experiences.",
+    "Streamline your operations with Business Process Outsourcing (BPO) services by Dexnive that is designed to reduce usual costs and improve experiences.",
 };
 
 const page = () => {
